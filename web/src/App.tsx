e25406de0c72cebe2ced6d7033e0300befa9011f
Olivia-Andriamihaja <medanolivia@gmail.com>
2026-02-10 06:34:50 +0100

    fixation des probleme lier au fichier du dossier carte

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import ManagerLogin from './ManagerLogin';
 import ManagerDashboard from './ManagerDashboard';
 import VisitorDashboard from './VisitorDashboard';
 import './App.css';
+import MapComponent from './MapComponent';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<ManagerLogin />} />
           <Route path="/dashboard" element={<ManagerDashboard />} />
           <Route path="/visiteur" element={<VisitorDashboard />} />
+          <Route path="/map" element={<MapComponent />} />
         </Routes>
       </div>
     </Router>
